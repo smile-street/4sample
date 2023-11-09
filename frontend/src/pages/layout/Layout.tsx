f9ecf0bba9fa7c34499cc4b9d1950f852aa99a89
Gohil <viran.gohil@fujitsu.com>
2023-11-09 09:53:27 +0000

    Your commit message1c

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -13,13 +13,13 @@ const shareButtonStyles: ICommandBarStyles & IButtonStyles = {
       width: 86,
       height: 32,
       borderRadius: 4,
-      background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)',
+      background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #E60012 33.63%, #E60012 70.31%, #E60012 100%)',
     //   position: 'absolute',
     //   right: 20,
       padding: '5px 12px',
       marginRight: '20px'
     },
-    icon: {
+    icon: {s
       color: '#FFFFFF',
     },
     rootHovered: {
